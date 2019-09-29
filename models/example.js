8929668e8b9c6fa8eadbf6fc6a67daa227f5c3e1
RedChristian77 <51823396+RedChristian77@users.noreply.github.com>
2019-09-28 23:03:24 -0500

    Fixing up the Models

--- models/example.js
+++ models/example.js
@@ -1,7 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
+  var Equipment = sequelize.define("Equipment", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.Integer
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     description: DataTypes.TEXT
   });
-  return Example;
+  return Equipment;
 };
