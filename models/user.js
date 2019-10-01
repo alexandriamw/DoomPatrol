cff7c6d27fb4b3b00437e82c41b16369f52b9146
CMK <ckrnke@gmail.com>
2019-10-01 13:43:47 -0500

    qqqqq

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    "User",
+    "Users",
     {
       accountName: DataTypes.STRING,
       hashedPW: DataTypes.STRING,
